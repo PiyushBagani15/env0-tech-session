9163cbe4415cd340619a8b227c7326b3cd7cb1b8
PiyushBagani15 <piyushbagani15@gmail.com>
2024-08-03 21:35:42 +0530

    small fixes

--- main.tf
+++ main.tf
@@ -77,21 +77,21 @@ resource "aws_route_table_association" "env0-rta" {
   route_table_id = aws_route_table.env0-rt.id
 }
 
-# data "aws_ami" "ubuntu" {
-#   most_recent = true
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-#   filter {
-#     name   = "name"
-#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-#   }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-#   owners = ["099720109477"] # Canonical
-# }
+  owners = ["099720109477"] # Canonical
+}
 
 
 
@@ -107,7 +107,7 @@ resource "aws_eip_association" "env0-eipa" {
 }
 
 resource "aws_instance" "env0-instance" {
-  ami                         = "ami-022ce6f32988af5fa"
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.env0-key-pair.key_name
   associate_public_ip_address = true
