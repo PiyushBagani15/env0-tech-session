dbdbc4cd8e5ed61d81e114d3b52a3710bcd4e054
PiyushBagani15 <piyushbagani15@gmail.com>
2024-08-03 20:12:33 +0530

    added outputs.tf file

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,11 @@
 output "url" {
-  value = "http://${aws_eip.env0.public_dns}"
+  value = "http://${aws_eip.env0-eip.public_dns}"
 }
 
 output "public_ip" {
-  value = aws_eip.env0.public_ip
+  value = aws_eip.env0-eip.public_ip
 }
-
 output "private_key" {
-  value     = tls_private_key.env0.private_key_pem
+  value     = tls_private_key.env0-key.private_key_pem
   sensitive = true
 }
